3c057273e02da0c1a739c34d522f42784c0cd485
Nachichuri <nachichuri@gmail.com>
2022-01-26 11:51:57 -0300

    Cambio webdriver a Gecko

--- Selenium.py
+++ Selenium.py
@@ -3,18 +3,19 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 
 url = r'https://visahome.prismamediosdepago.com/socios/service/giftCard'
 
 
 def get_balance(card_num):
     # We create the driver
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')
+    service = Service(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service, options=options)
 
     # Go to the webpage
     driver.get(url)
